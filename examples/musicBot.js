b9d02a4e0f2ad8a3473f64d0ffa0adbe60782192
HectorsLittleDevil <38656944+HectorsLittleDevil@users.noreply.github.com>
2018-04-24 20:13:35 +0200

    Update musicBot.js

--- examples/musicBot.js
+++ examples/musicBot.js
@@ -1,7 +1,7 @@
-const Client = require('discord.js');
+const Client = require('discord.js');.Client;
 const Auth = require('./auth.json');
 
-const music = require('../');
+const music = require('discord.js-music');
 
 // Create a new client.
 const client = new Client();
