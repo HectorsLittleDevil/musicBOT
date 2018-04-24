bf8e64814c77d4886bf016322fae4c35425e69d4
HectorsLittleDevil <38656944+HectorsLittleDevil@users.noreply.github.com>
2018-04-24 20:12:05 +0200

    Create Discord.js

---
+++ Discord.js
@@ -0,0 +1,7 @@
+const Client = require('discord.js').Client;
+const music = require('discord.js-music');
+
+const client = new Client();
+music(client);
+
+client.loginWithToken('< NDM3NzA2NTIzNTgyMzk4NDY0.DcEDsg.gA4-Vzzxf1aag2LPBMFTnhSZfHU >');
